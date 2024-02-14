5387edd52a84f18693a4726c5499c38a5edf4b9d
fork-hnk <111276880+fork-hnk@users.noreply.github.com>
2024-02-14 12:25:41 +0100

    Add alternative methods to get LocalIP in Start_TURNServer.ps1.

--- SignallingWebServer/platform_scripts/cmd/Start_TURNServer.ps1
+++ SignallingWebServer/platform_scripts/cmd/Start_TURNServer.ps1
@@ -6,7 +6,14 @@ set_start_default_values "y" "n" # Set both TURN and STUN server defaults
 use_args($args)
 print_parameters
 #$LocalIp = Invoke-WebRequest -Uri "http://169.254.169.254/latest/meta-data/local-ipv4"
-$LocalIP = (Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address).IPV4Address.IPAddressToString
+#$LocalIP = (Test-Connection -ComputerName (hostname) -Count 1  | Select IPV4Address).IPV4Address.IPAddressToString
+
+# LocalIP Alternative: Get network adapter based on name.
+$targetAdapter = Get-NetAdapter -Physical | Where-Object { $_.Status -eq 'Up' -and $_.Name -like 'Ethernet' } | Select -First 1
+$localIP = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $targetAdapter.IfIndex).IPAddress
+
+# LocalIP Alternative: get Ip by matching expected range 192.168.1.*
+#$LocalIP = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -like '192.168.1.*' } | Select -First 1).IPAddress
 
 Write-Output "Private IP: $LocalIP"
 
